eb905c65fd4df4b485bc3601295767c243424c31
21DP2RJerm <100911489+21DP2RJerm@users.noreply.github.com>
2022-06-03 11:09:11 +0300

    Add files via upload

---
+++ Statistika.py
@@ -0,0 +1,28 @@
+print("Šīs sistēmas mērķis ir iemācīt un pārbaudīt tavas zināšanas par tēmām:")
+print("Aritmētiskais vidējais")
+print("Moda")
+print("Mediāna")
+
+def arit_vid_teorija():
+    print("Aritmētiskais vidējais ir vienāds ar pētījuma datu kopas visu vērtību summas un pētījuma novērojumu skaita dalījumu")
+    print('xVID = (X1 + X2 +...Xn) / n')
+    print("n ir visu datu kopējais skaits")
+    print("Nospied Enter taustiņu lai lasītu tālāk")
+    input()
+    print("Piemēram ja mēs aizgājām uz 5 veikaliem lai pārbaudītu maizes cenu un uzzinātu kāds ir vidējais aritmētiskais baltmaizes cenai un ieguvām cenas: ")
+    print("0,78 euro")
+    print("0,67 euro")
+    print("0,72 euro")
+    print("0,59 euro")
+    print("0,81 euro")
+    print("Tad n = 5 jo ievākti dati par cenām no pieciem veikaliem")
+    print("Lai aprēķinātu vidējo aritmētisko jāsakaita visas cenas kopā un jāizdala ar n")
+    cenas_summa = 3,57
+    summa_ievade = float(input("Ievadi visu cenu summu: ")
+    while atrisinats == False:
+	if summa_ievade = cenas_summa:
+	    print("Pareizi")
+    		atrisinats = True
+	else:
+	    print("Nepareizi mēģini vēlreiz")
+	    summa_ievade = float(input("Ievadi visu cenu summu: ")
